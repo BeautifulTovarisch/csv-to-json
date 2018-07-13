a02a1392d566f94619b062ce9c3fb9ae1af3a67d
Anthony <re@towson.edu>
2018-07-13 18:31:01 -0400

    using cheshire to write json, removed reducers

--- project.clj
+++ project.clj
@@ -3,9 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/data.csv "0.1.4"]
-                 [org.clojure/data.json "0.2.6"]]
+  :dependencies [[cheshire "5.8.0"]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.csv "0.1.4"]]
+                 
   :main ^:skip-aot usaspending-etl.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/usaspending_etl/csv_reader.clj
+++ src/usaspending_etl/csv_reader.clj
@@ -2,26 +2,33 @@
   (:gen-class)
   (:require [clojure.java.io :as io]
             [clojure.data.csv :as csv]
-            [clojure.data.json :as json]
-            [clojure.core.reducers :as r]))
+            [cheshire.core :refer [generate-stream
+                                   generate-string] :as json ]))
+
+(defn format-post [documents]
+  (json/generate-string { :docs documents }))
 
 (defn read-csv [reader]
   (csv/read-csv reader))
 
-(defn csv->map [data]
-  (r/map #(zipmap (first data) %) (rest data)))
+(defn row->map [data]
+  (map #(zipmap (first data) %) (rest data)))
 
-(defn map->json [data]
-  (r/map json/write-str data))
+(defn rows->json [data]
+  (map json/generate-string data))
 
-(defn write-json [writer json]
-  (doseq [line json] (.write writer line)))
+(defn write-json [dest json]
+  (generate-stream json (io/writer dest)))
 
-(defn csv->json []
-  (with-open [reader (io/reader "data/subawards.csv")
-              writer (io/writer "data/subawards.json" :append true)]
+(defn csv->json
+  ([source]
+   (csv->json source (clojure.string/replace source #".csv" ".json")))
+
+  ([source dest]
+  (with-open [reader (io/reader source)
+              writer (io/writer dest)]
     (->> (read-csv reader)
-         (csv->map)
-         (map->json)
-         (into [])
-         (write-json writer))))
+         (row->map)
+         (rows->json)
+         (format-post)
+         (write-json dest)))))
